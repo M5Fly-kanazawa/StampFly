72c156a641a9b712572d6853f3e4313f041646f9
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2023-07-24 22:00:18 +0900

    update

--- src/flight_control.cpp
+++ src/flight_control.cpp
@@ -30,9 +30,9 @@ const float Control_period = 0.0025f;//400Hz
 
 //PID Gain
 //Rate control PID gain
-const float Roll_rate_kp = 0.75f;//0.65
+const float Roll_rate_kp = 0.6f;//0.65
 const float Roll_rate_ti = 0.7f;
-const float Roll_rate_td = 0.025f;//0.03
+const float Roll_rate_td = 0.01;//0.03
 const float Roll_rate_eta = 0.125f;
 
 const float Pitch_rate_kp = 0.75f;//0.65
@@ -158,6 +158,8 @@ void telemetry(void);
 void float2byte(float x, uint8_t* dst);
 void append_data(uint8_t* data , uint8_t* newdata, uint8_t index, uint8_t len);
 void data2log(uint8_t* data_list, float add_data, uint8_t index);
+void make_telemetry_data(uint8_t* senddata);
+void make_telemetry_header_data(uint8_t* senddata);
 
 //割り込み関数
 //Intrupt function
@@ -779,19 +781,36 @@ uint8_t get_flip_button(void)
   return state;
 }
 
+const uint8_t MAXINDEX=98;
 
 void telemetry(void)
 {
-  //Telemetry
-  const uint8_t MAXINDEX=98;
-  float d_float;
-  uint8_t d_int[4];
   uint8_t senddata[MAXINDEX]; 
-  uint8_t index=0;  
 
+  //Telemetry
   if(Telem_mode==0)
   {
     Telem_mode = 1;
+    make_telemetry_header_data(senddata);
+
+    //Send !
+    telemetry_send(senddata, sizeof(senddata));
+  }  
+  else if(Mode > AVERAGE_MODE)
+  {
+    make_telemetry_data(senddata);
+    //Send !
+    if(telemetry_send(senddata, sizeof(senddata))==1)esp_led(0x110000, 1);//Telemetory Reciver OFF
+    else esp_led(0x001100, 1);//Telemetory Reciver ON
+  }
+}
+
+void make_telemetry_header_data(uint8_t* senddata)
+{
+  float d_float;
+  uint8_t d_int[4];
+  uint8_t index=0;  
+
     index=2;
     for (uint8_t i=0;i<(MAXINDEX-2)/4;i++)
     {
@@ -929,94 +948,94 @@ void telemetry(void)
     //append_data(senddata, d_int, index, 4);
     index = index + 4;
 
-    //Send !
-    telemetry_send(senddata, sizeof(senddata));
-  }  
-  else if(Mode > AVERAGE_MODE)
-  {
-    //Telemetry Header
-    senddata[0]=88;
-    senddata[1]=88;
-    index = 2;
-    //1 Time
-    data2log(senddata, Elapsed_time, index);
-    index = index + 4;
-    //2 delta Time
-    data2log(senddata, Interval_time, index);
-    index = index + 4;
-    //3 Roll_angle
-    data2log(senddata, (Roll_angle-Roll_angle_offset)*180/PI, index);
-    index = index + 4;
-    //4 Pitch_angle
-    data2log(senddata, (Pitch_angle-Pitch_angle_offset)*180/PI, index);
-    index = index + 4;
-    //5 Yaw_angle
-    data2log(senddata, (Yaw_angle-Yaw_angle_offset)*180/PI, index);
-    index = index + 4;
-    //6 P
-    data2log(senddata, (Roll_rate)*180/PI, index);
-    index = index + 4;
-    //7 Q
-    data2log(senddata, (Pitch_rate)*180/PI, index);
-    index = index + 4;
-    //8 R
-    data2log(senddata, (Yaw_rate)*180/PI, index);
-    index = index + 4;
-    //9 Roll_angle_reference
-    data2log(senddata, Roll_angle_reference*180/PI, index);
-    //data2log(senddata, 0.5f * 180.0f *Roll_angle_command, index);
-    index = index + 4;
-    //10 Pitch_angle_reference
-    data2log(senddata, Pitch_angle_reference*180/PI, index);
-    //data2log(senddata, 0.5 * 189.0f* Pitch_angle_command, index);
-    index = index + 4;
-    //11 P ref
-    data2log(senddata, Roll_rate_reference*180/PI, index);
-    index = index + 4;
-    //12 Q ref
-    data2log(senddata, Pitch_rate_reference*180/PI, index);
-    index = index + 4;
-    //13 R ref
-    data2log(senddata, Yaw_rate_reference*180/PI, index);
-    index = index + 4;
-    //14 T ref
-    data2log(senddata, Thrust_command/BATTERY_VOLTAGE, index);
-    index = index + 4;
-    //15 Voltage
-    data2log(senddata, Voltage, index);
-    index = index + 4;
-    //16 Accel_x_raw
-    data2log(senddata, Accel_x_raw, index);
-    index = index + 4;
-    //17 Accel_y_raw
-    data2log(senddata, Accel_y_raw, index);
-    index = index + 4;
-    //18 Accel_z_raw
-    data2log(senddata, Accel_z_raw, index);
-    index = index + 4;
-    //19 Acc Norm
-    data2log(senddata, Acc_norm, index);
-    index = index + 4;
-    //20 FrontRight_motor_duty
-    data2log(senddata, FrontRight_motor_duty, index);
-    index = index + 4;
-    //21 FrontLeft_motor_duty
-    data2log(senddata, FrontLeft_motor_duty, index);
-    index = index + 4;
-    //22 RearRight_motor_duty
-    data2log(senddata, RearRight_motor_duty, index);
-    index = index + 4;
-    //23 RearLeft_motor_duty
-    data2log(senddata, RearLeft_motor_duty, index);
-    index = index + 4;
-    //24 Altitude2
-    data2log(senddata, Altitude2, index);
-    index = index + 4;
+}
 
-    //Send !
-    if(telemetry_send(senddata, sizeof(senddata))==1)esp_led(0x110000, 1);//Telemetory Reciver OFF
-    else esp_led(0x001100, 1);//Telemetory Reciver ON
-  }
+void make_telemetry_data(uint8_t* senddata)
+{
+  const uint8_t MAXINDEX=98;
+  float d_float;
+  uint8_t d_int[4];
+  //uint8_t senddata[MAXINDEX]; 
+  uint8_t index=0;  
+
+  //Telemetry Header
+  senddata[0]=88;
+  senddata[1]=88;
+  index = 2;
+  //1 Time
+  data2log(senddata, Elapsed_time, index);
+  index = index + 4;
+  //2 delta Time
+  data2log(senddata, Interval_time, index);
+  index = index + 4;
+  //3 Roll_angle
+  data2log(senddata, (Roll_angle-Roll_angle_offset)*180/PI, index);
+  index = index + 4;
+  //4 Pitch_angle
+  data2log(senddata, (Pitch_angle-Pitch_angle_offset)*180/PI, index);
+  index = index + 4;
+  //5 Yaw_angle
+  data2log(senddata, (Yaw_angle-Yaw_angle_offset)*180/PI, index);
+  index = index + 4;
+  //6 P
+  data2log(senddata, (Roll_rate)*180/PI, index);
+  index = index + 4;
+  //7 Q
+  data2log(senddata, (Pitch_rate)*180/PI, index);
+  index = index + 4;
+  //8 R
+  data2log(senddata, (Yaw_rate)*180/PI, index);
+  index = index + 4;
+  //9 Roll_angle_reference
+  data2log(senddata, Roll_angle_reference*180/PI, index);
+  //data2log(senddata, 0.5f * 180.0f *Roll_angle_command, index);
+  index = index + 4;
+  //10 Pitch_angle_reference
+  data2log(senddata, Pitch_angle_reference*180/PI, index);
+  //data2log(senddata, 0.5 * 189.0f* Pitch_angle_command, index);
+  index = index + 4;
+  //11 P ref
+  data2log(senddata, Roll_rate_reference*180/PI, index);
+  index = index + 4;
+  //12 Q ref
+  data2log(senddata, Pitch_rate_reference*180/PI, index);
+  index = index + 4;
+  //13 R ref
+  data2log(senddata, Yaw_rate_reference*180/PI, index);
+  index = index + 4;
+  //14 T ref
+  data2log(senddata, Thrust_command/BATTERY_VOLTAGE, index);
+  index = index + 4;
+  //15 Voltage
+  data2log(senddata, Voltage, index);
+  index = index + 4;
+  //16 Accel_x_raw
+  data2log(senddata, Accel_x_raw, index);
+  index = index + 4;
+  //17 Accel_y_raw
+  data2log(senddata, Accel_y_raw, index);
+  index = index + 4;
+  //18 Accel_z_raw
+  data2log(senddata, Accel_z_raw, index);
+  index = index + 4;
+  //19 Acc Norm
+  data2log(senddata, Acc_norm, index);
+  index = index + 4;
+  //20 FrontRight_motor_duty
+  data2log(senddata, FrontRight_motor_duty, index);
+  index = index + 4;
+  //21 FrontLeft_motor_duty
+  data2log(senddata, FrontLeft_motor_duty, index);
+  index = index + 4;
+  //22 RearRight_motor_duty
+  data2log(senddata, RearRight_motor_duty, index);
+  index = index + 4;
+  //23 RearLeft_motor_duty
+  data2log(senddata, RearLeft_motor_duty, index);
+  index = index + 4;
+  //24 Altitude2
+  data2log(senddata, Altitude2, index);
+  index = index + 4;
 }
 
 void data2log(uint8_t* data_list, float add_data, uint8_t index)
