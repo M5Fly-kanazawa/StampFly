dc75cbb8fae91bcb4a5548241cb9da3cb948fb47
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2023-08-05 17:32:26 +0900

    update

--- src/alt_kalman.cpp
+++ src/alt_kalman.cpp
@@ -7,15 +7,32 @@ Alt_kalman::Alt_kalman(){};
 
 void Alt_kalman::update(double z_sens, double accel)
 {
+        //x:estimate x_:predict
+
+        //predict state
+        velocity_ = velocity + accel*step;
+        altitude_ = altitude + velocity*step;
+        //velocity = velocity_;
+        //altitude = altitude_; 
+
+        //predict P
+        p11_ = p11 + q1;
+        p12_ = step*p11 + p12;
+        p21_ = step*p11 + p21;
+        p22_ = step*step*p11 + step*p21 + step*p12 + p22 + q2;
+
+
         //update kalman gain
-        k1 = p12/(p22+R);
-        k2 = p22/(p22+R);
+        float s = p22_ + R;
+        k1 = p12_ / s;
+        k2 = p22_ / s;
+
+        //inovation
+        float e = z_sens - altitude_; 
 
         //estimate state
-        velocity_ = velocity + k1*(z_sens - altitude);
-        altitude_ = altitude + k2*(z_sens - altitude);
-        velocity = velocity_;
-        altitude = altitude_;
+        velocity = velocity_ + k1 * e;
+        altitude = altitude_ + k2 * e;
         
         //Estimated state (output)
         Velocity = velocity;
@@ -23,31 +40,11 @@ void Alt_kalman::update(double z_sens, double accel)
         //printf("%11.3f %11.4f %11.4f %11.4f %11.4f ", t+step, velocity, altitude, z_sens, true_v);
 
         //estimate P
-        p11_ = p11 - k1*p21;
-        p12_ = p12 - k1*p22;
-        p21_ = p21 - k2*p21;
-        p22_ = p22 - k2*p22;
-        p11 = p11_;
-        p12 = p12_;
-        p21 = p21_;
-        p22 = p22_;
+        p11 = p11_ - k1 * p21_;
+        p12 = p12_ - k1 * p22_;
+        p21 = p21_ - k2 * p21_;
+        p22 = p22_ - k2 * p22_;
         //printf("%11.4f %11.4f %11.4f %11.4f\n", p11, p12, p21, p22);
-
-        //predict state
-        velocity_ = velocity + accel*step;
-        altitude_ = altitude + velocity*step;
-        velocity = velocity_;
-        altitude = altitude_; 
-
-        //predict P
-        p11_ = p11 + step*step*q11;
-        p12_ = step*p11 + p12;
-        p21_ = step*p11 + p21;
-        p22_ = step*step*p11 + step*p21 + step*p12 + p22 + step*step*q22;
-        p11 = p11_;
-        p12 = p12_;
-        p21 = p21_;
-        p22 = p22_;
 }
 
 
--- src/alt_kalman.hpp
+++ src/alt_kalman.hpp
@@ -4,39 +4,38 @@
 class Alt_kalman
 {
     //accel
-    double accel=0.0;
+    float accel=0.0;
 
     //state
-    double velocity=0.0, altitude=0.0;
-    double velocity_, altitude_;
-    double true_v;
+    float velocity=0.0, altitude=0.0;
+    float velocity_, altitude_;
+    float true_v;
 
     //Sensor
-    double z_sens;
+    float z_sens;
 
     //Kalman Gain
-    double k1,k2;
+    float k1,k2;
 
     //F
-    double f11=1.0, f12=0.0, f21=step, f22=1.0;
+    float f11=1.0, f12=0.0, f21=step, f22=1.0;
 
     //H
     double h1=0.0, h2=1.0;
 
-    
     //P
-    double p11=10.0, p12=10.0, p21=10.0, p22=10.0;
+    double p11=1.0, p12=1.0, p21=1.0, p22=1.0;
     double p11_, p12_, p21_, p22_;
 
     //Q
-    double q11=1.0, q12=0.0, q21=0.0, q22 =1.0;
+    double q1=(1.0e-3)*(1.0e-3), q2=(1.0e-3)*(1.0e-3);
 
     //R
-    double R = 0.1;
+    double R = 0.02*0.02;
 
     public:
     //setep
-    double step=0.05;
+    double step=1/30;
     //state
     double Velocity=0.0, Altitude=0.0;
     Alt_kalman();
